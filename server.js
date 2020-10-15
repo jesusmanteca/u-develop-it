752b2822ca534a025f0c913ff596f5936b87ec1d
Jesus Manteca <jesusmanteca@gmail.com>
2020-10-15 15:19:46 -0500

    add post route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -55,6 +56,7 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
+// \/ \/ \/ \/ \/ \/ \/ \/ \/
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -71,6 +73,39 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 });
+// /\ /\ /\ /\ /\ /\ /\ /\ /\
+
+// \/ \/ \/ \/ \/ \/ \/ \/ \/
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  
+  // ES5 function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: body,
+      id: this.lastID
+    });
+  });
+
+
+});
+// /\ /\ /\ /\ /\ /\ /\ /\ /\
 
 // \/ \/ \/ \/ \/ \/ \/ \/ \/
 // Get all candidates //
@@ -89,7 +124,7 @@ app.get('/api/candidates', (req, res) => {
       });
     });
   });
-  // /\ /\ /\ /\ /\ /\ /\ /\ /\
+// /\ /\ /\ /\ /\ /\ /\ /\ /\
 
 // \/ \/ \/ \/ \/ \/ \/ \/ \/
 //        CATCH ALL        //
