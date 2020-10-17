2810e8d4afa83b4b88911def670559b74c68a28d
Jesus Manteca <jesusmanteca@gmail.com>
2020-10-17 09:43:16 -0500

    added join to merge tables

--- server.js
+++ server.js
@@ -40,8 +40,12 @@ app.get('/', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates 
-               WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
@@ -110,9 +114,13 @@ app.post('/api/candidate', ({ body }, res) => {
 // \/ \/ \/ \/ \/ \/ \/ \/ \/
 // Get all candidates //
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
@@ -123,7 +131,7 @@ app.get('/api/candidates', (req, res) => {
         data: rows
       });
     });
-  });
+});
 // /\ /\ /\ /\ /\ /\ /\ /\ /\
 
 // \/ \/ \/ \/ \/ \/ \/ \/ \/
